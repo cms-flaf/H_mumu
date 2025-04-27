9d9144cfa41a5fd850ef7a81967a031aa065b127
Valeria D'Amante <valeriadamante@gmail.com>
2025-04-27 13:05:51 +0200

    created producer for NNInputs

--- include/HmumuCore.h
+++ include/HmumuCore.h
@@ -13,3 +13,22 @@ struct VBFJets
   float eta_separation;
   bool isVBF;
 };
+
+
+float pT_sum (const RVecLV & all_p4s ) {
+  float pT_x_sum = 0.;
+  float pT_y_sum = 0.;
+  for (int idx = 0; idx < all_p4s.size(); idx++){
+    pT_x_sum+=all_p4s[idx].Px();
+    pT_y_sum+=all_p4s[idx].Py();
+  }
+  return sqrt(pT_x_sum*pT_x_sum + pT_y_sum*pT_y_sum);
+}
+
+float pT_diff (const LorentzVectorM & p4_1,const LorentzVectorM & p4_2) {
+  float delta_px = p4_1.Px() - p4_2.Px();
+  float delta_py = p4_1.Py() - p4_2.Py();
+  return sqrt(delta_px * delta_px + delta_py * delta_py);
+}
+
+
