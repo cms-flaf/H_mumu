680f9b4ea7c4fe92ba7793cbd2a3792df631a3be
Alexander Yeagle <ayeagle@lxplus902.cern.ch>
2025-06-11 01:34:29 +0200

    Implement mlphep for CMS style testing plots

--- Studies/DNN/model_generation/test.py
+++ Studies/DNN/model_generation/test.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
+import mplhep
 import numpy as np
 import pandas as pd
 import torch
 from tqdm import tqdm
 
 from sample_type_lookup import lookup
+
+mplhep.style.use(mplhep.styles.CMS)
 
 class Tester:
 
@@ -37,21 +40,13 @@ class Tester:
         results = self.testing_df
         signal = results[results.Label == 1].NN_Output
         background = results[results.Label == 0].NN_Output
-        h1 = plt.hist(
-            signal, range=(0, 1), bins=50, label="signal", color="tab:blue", alpha=0.6
-        )
-        h2 = plt.hist(
-            background,
-            range=(0, 1),
-            bins=50,
-            label="background",
-            color="tab:orange",
-            alpha=0.6,
-        )
+        h1 = np.histogram(signal, range=(0, 1), bins=50)
+        h2 = np.histogram(background, range=(0, 1), bins=50)
+        mplhep.histplot([h1,h2], label=['Signal','Background'], stack=True)
         if log:
             plt.yscale("log")
-        plt.xlabel("NN output")
-        plt.ylabel("Count (#)")
+        plt.xlabel("Network output")
+        plt.ylabel("Events")
         plt.legend()
         if log:
             output_name = "model_hist_log.png"
@@ -66,32 +61,24 @@ class Tester:
     def make_stackplot(self, log=False, show=False):
         # Init plot
         plt.clf()
-        fig, (ax_plot, ax_legend) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [3, 1]})
-
-	df = self.testing_df
+        df = self.testing_df
         # Add individual hist curves
+        hists = []
+        labels=[]
         for p in sorted(pd.unique(df.sample_type)):
-            selected = df[df. == p]
-            counts, bin_edges = np.histogram(selected.NN_Output, bins=50, range=(0,1))
-            x = np.concatenate(([0], bin_edges))
-            y = np.concatenate(([0], counts, [0]))
-            name = lookup[p]
-            ax_plot.plot(x, y, label=name, drawstyle='steps-post')
+            selected = df[df.sample_type == p]
+            h = np.histogram(selected.NN_Output, bins=50, range=(0,1))
+            hists.append(h)
+            labels.append(lookup[p])
+        # Create plot
+        mplhep.histplot(hists, label=labels, stack=True)
         # Plot config
-        ax_plot.set_xlim((0,1))
         if log:
-            ax_plot.set_yscale("log")
-        else:
-            ax_plot.set_ylim(bottom=0)
-        ax_plot.set_xlabel("NN Output")
-        ax_plot.set_ylabel("Count (#)")
-        # Create legend subplot
-        h, l = ax_plot.get_legend_handles_labels()
-        ax_legend.legend(h, l, loc='center', frameon=False, fontsize='medium')
-        ax_legend.axis('off')
-        # Output
-        plt.tight_layout()
+            plt.yscale("log")
+        plt.xlabel("Network output")
+        plt.ylabel("Events")
+        plt.legend()
         if show:
             plt.show()
         else:
-            fig.savefig("stackplot.png", bbox_inches="tight")
+            plt.savefig("stackplot.png", bbox_inches="tight")
