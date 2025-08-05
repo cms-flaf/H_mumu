748bec18e42334d904a70c4fb8bd83e0733636c6
Valeria D'Amante <46978288+valeriadamante@users.noreply.github.com>
2025-08-05 15:46:13 +0200

    Private samples inclusion (#14)
    * compativility with ScaRe
    
    * inclusion of all eras
    
    * fixed bugs in jet veto map + added private samples
    
    * included weights and new samples
    
    * added remote fs + JVM
    
    * added helpers for hist tuples
    
    * added treeName + HistTupleDef (for future) in global config
    
    * updated FLAF
    
    * updated submodules to current PRs
    
    * updated FLAF to correct branch
    
    * changed histHelper to old location + added saples
    
    * added new config entries
    
    * added formatting rules
    
    * fixed python formatting
    
    * restore make stackplot
    
    * fixed yaml formatting
    
    * fixed yaml formatting 2
    
    * removed formatting rules
    
    * check yaml format
    
    * tmp add of formatting configs
    
    * formatting yaml 3
    
    * formatting yaml 4
    
    * formatting yaml 5
    
    * fixed indentation bug
    
    * updated submodules
    
    * updated submodules 2
    
    * yaml formatting 2
    
    * yaml formatting 3
    
    * yaml formatting 4
    
    * yaml formatting 5
    
    * yaml formatting 6

--- AnaProd/anaTupleDef.py
+++ AnaProd/anaTupleDef.py
@@ -3,85 +3,383 @@ import FLAF.Common.BaselineSelection as CommonBaseline
 from Corrections.Corrections import Corrections
 from FLAF.Common.Utilities import *
 
-lepton_legs = [ "mu1", "mu2" ]
-offline_legs = [ "mu1", "mu2" ]
+lepton_legs = ["mu1", "mu2"]
+offline_legs = ["mu1", "mu2"]
 loadTF = False
 loadHHBtag = False
 
 
-Muon_observables = ["IP_cov00","IP_cov10","IP_cov11","IP_cov20","IP_cov21","IP_cov22","IPx","IPy","IPz","bField_z","bsConstrainedChi2","bsConstrainedPt","bsConstrainedPtErr","charge","dxy","dxyErr","dxybs","dz","dzErr","eta","fsrPhotonIdx","genPartFlav","genPartIdx","highPtId","highPurity","inTimeMuon","ip3d","ipLengthSig","isGlobal","isPFcand","isStandalone","isTracker","jetIdx","jetNDauCharged","jetPtRelv2","jetRelIso","looseId","mass","mediumId","mediumPromptId","miniIsoId","miniPFRelIso_all","miniPFRelIso_chg","multiIsoId","mvaLowPt","mvaMuID","mvaMuID_WP","nStations","nTrackerLayers","pdgId","pfIsoId","pfRelIso03_all","pfRelIso03_chg","pfRelIso04_all","phi","promptMVA","pt","ptErr","puppiIsoId","segmentComp","sip3d","softId","softMva","softMvaId","softMvaRun3","svIdx","tightCharge","tightId","tkIsoId","tkRelIso","track_cov00","track_cov10","track_cov11","track_cov20","track_cov21","track_cov22","track_cov30","track_cov31","track_cov32","track_cov33","track_cov40","track_cov41","track_cov42","track_cov43","track_cov44","track_dsz","track_dxy","track_lambda","track_phi","track_qoverp","triggerIdLoose","tunepRelPt"]
-# Electron_observables = ["Electron_mvaNoIso_WP80", "Electron_mvaIso_WP80","Electron_pfRelIso03_all"]
-JetObservables = ["PNetRegPtRawCorr","PNetRegPtRawCorrNeutrino","PNetRegPtRawRes","area","btagDeepFlavB","btagDeepFlavCvB","btagDeepFlavCvL","btagDeepFlavQG","btagPNetB","btagPNetCvB","btagPNetCvL","btagPNetCvNotB","btagPNetQvG","btagPNetTauVJet","chEmEF","chHEF","chMultiplicity","electronIdx1","electronIdx2","eta","genJetIdx","hadronFlavour","hfEmEF","hfHEF","hfadjacentEtaStripsSize","hfcentralEtaStripSize","hfsigmaEtaEta","hfsigmaPhiPhi","jetId","mass","muEF","muonIdx1","muonIdx2","muonSubtrFactor","nConstituents","nElectrons","nMuons","nSVs","neEmEF","neHEF","neMultiplicity","partonFlavour","phi","pt","rawFactor","svIdx1","svIdx2"]
+Muon_observables = [
+    "IP_cov00",
+    "IP_cov10",
+    "IP_cov11",
+    "IP_cov20",
+    "IP_cov21",
+    "IP_cov22",
+    "IPx",
+    "IPy",
+    "IPz",
+    "bField_z",
+    "bsConstrainedChi2",
+    "bsConstrainedPt",
+    "bsConstrainedPtErr",
+    "charge",
+    "dxy",
+    "dxyErr",
+    "dxybs",
+    "dz",
+    "dzErr",
+    "eta",
+    "fsrPhotonIdx",
+    "genPartFlav",
+    "genPartIdx",
+    "highPtId",
+    "highPurity",
+    "inTimeMuon",
+    "ip3d",
+    "ipLengthSig",
+    "isGlobal",
+    "isPFcand",
+    "isStandalone",
+    "isTracker",
+    "jetIdx",
+    "jetNDauCharged",
+    "jetPtRelv2",
+    "jetRelIso",
+    "looseId",
+    "mass",
+    "mediumId",
+    "mediumPromptId",
+    "miniIsoId",
+    "miniPFRelIso_all",
+    "miniPFRelIso_chg",
+    "multiIsoId",
+    "mvaLowPt",
+    "mvaMuID",
+    "mvaMuID_WP",
+    "nStations",
+    "nTrackerLayers",
+    "pdgId",
+    "pfIsoId",
+    "pfRelIso03_all",
+    "pfRelIso03_chg",
+    "pfRelIso04_all",
+    "phi",
+    "promptMVA",
+    "pt",
+    "ptErr",
+    "puppiIsoId",
+    "segmentComp",
+    "sip3d",
+    "softId",
+    "softMva",
+    "softMvaId",
+    "softMvaRun3",
+    "svIdx",
+    "tightCharge",
+    "tightId",
+    "tkIsoId",
+    "tkRelIso",
+    "track_cov00",
+    "track_cov10",
+    "track_cov11",
+    "track_cov20",
+    "track_cov21",
+    "track_cov22",
+    "track_cov30",
+    "track_cov31",
+    "track_cov32",
+    "track_cov33",
+    "track_cov40",
+    "track_cov41",
+    "track_cov42",
+    "track_cov43",
+    "track_cov44",
+    "track_dsz",
+    "track_dxy",
+    "track_lambda",
+    "track_phi",
+    "track_qoverp",
+    "triggerIdLoose",
+    "tunepRelPt",
+]
 
-JetObservablesMC = ["hadronFlavour","partonFlavour"]
+PrimaryVertexObservables = [
+    "PVBS_chi2",
+    "PVBS_cov00",
+    "PVBS_cov10",
+    "PVBS_cov11",
+    "PVBS_cov20",
+    "PVBS_cov21",
+    "PVBS_cov22",
+    "PVBS_x",
+    "PVBS_y",
+    "PVBS_z",
+    "PV_score",
+    "PV_z",
+    "PV_chi2",
+    "PV_ndof",
+    "PV_npvs",
+    "PV_npvsGood",
+    "PV_score",
+    "PV_sumpt2",
+    "PV_sumpx",
+    "PV_sumpy",
+    "PV_x",
+    "PV_y",
+    "PV_z",
+]
 
-FatJetObservables = ["area", "btagCSVV2", "btagDDBvLV2", "btagDeepB", "btagHbb", "deepTagMD_HbbvsQCD",
-                     "deepTagMD_ZHbbvsQCD", "deepTagMD_ZbbvsQCD", "deepTagMD_bbvsLight", "deepTag_H",
-                     "jetId", "msoftdrop", "nBHadrons", "nCHadrons", "nConstituents","rawFactor",
-                      "particleNetMD_QCD", "particleNetMD_Xbb", "particleNet_HbbvsQCD", "particleNet_mass", # 2018
-                     "particleNet_QCD","particleNet_XbbVsQCD", # 2016
-                     "particleNetLegacy_QCD", "particleNetLegacy_Xbb", "particleNetLegacy_mass", # 2016
-                     "particleNetWithMass_QCD", "particleNetWithMass_HbbvsQCD", "particleNet_massCorr", # 2016
-                     "ptRes", "idbtagPNetB"]
+SecondaryVertexObservables = [
+    "SV_jetIdx",
+    "SV_sVIdx",
+    "SV_charge",
+    "SV_chi2",
+    "SV_dlen",
+    "SV_dlenSig",
+    "SV_dxy",
+    "SV_dxySig",
+    "SV_eta",
+    "SV_mass",
+    "SV_ndof",
+    "SV_ntracks",
+    "SV_pAngle",
+    "SV_phi",
+    "SV_pt",
+    "SV_x",
+    "SV_y",
+    "SV_z",
+]
 
-FatJetObservablesMC = ["hadronFlavour","partonFlavour"]
+PUObservables = [
+    "Pileup_gpudensity",
+    "Pileup_nPU",
+    "Pileup_pthatmax",
+    "Pileup_pudensity",
+    "Pileup_sumEOOT",
+    "Pileup_sumLOOT",
+]
 
-SubJetObservables = ["btagDeepB", "eta", "mass", "phi", "pt", "rawFactor"]
-SubJetObservablesMC = ["hadronFlavour","partonFlavour"]
+JetObservables = [
+    "PNetRegPtRawCorr",
+    "PNetRegPtRawCorrNeutrino",
+    "PNetRegPtRawRes",
+    "UParTAK4RegPtRawCorr",
+    "UParTAK4RegPtRawCorrNeutrino",
+    "UParTAK4RegPtRawRes",
+    "area",
+    "btagDeepFlavB",
+    "btagDeepFlavCvB",
+    "btagDeepFlavCvL",
+    "btagDeepFlavQG",
+    "btagPNetB",
+    "btagPNetCvB",
+    "btagPNetCvL",
+    "btagPNetCvNotB",
+    "btagPNetQvG",
+    "btagPNetTauVJet",
+    "btagUParTAK4B",
+    "btagUParTAK4CvB",
+    "btagUParTAK4CvL",
+    "btagUParTAK4CvNotB",
+    "btagUParTAK4QvG",
+    "btagUParTAK4TauVJet",
+    "chEmEF",
+    "chHEF",
+    "chMultiplicity",
+    "electronIdx1",
+    "electronIdx2",
+    "eta",
+    "genJetIdx",
+    "hfEmEF",
+    "hfHEF",
+    "hfadjacentEtaStripsSize",
+    "hfcentralEtaStripSize",
+    "hfsigmaEtaEta",
+    "hfsigmaPhiPhi",
+    "jetId",
+    "mass",
+    "muEF",
+    "muonIdx1",
+    "muonIdx2",
+    "muonSubtrFactor",
+    "nConstituents",
+    "nElectrons",
+    "nMuons",
+    "nSVs",
+    "neEmEF",
+    "neHEF",
+    "neMultiplicity",
+    "phi",
+    "pt",
+    "puIdDisc",
+    "puId_beta",
+    "puId_dR2Mean",
+    "puId_frac01",
+    "puId_frac02",
+    "puId_frac03",
+    "puId_frac04",
+    "puId_jetR",
+    "puId_jetRchg",
+    "puId_majW",
+    "puId_minW",
+    "puId_nCharged",
+    "puId_ptD",
+    "puId_pull",
+    "rawFactor",
+    "svIdx1",
+    "svIdx2",
+    "ptRes",
+    "vetoMap",
+]
 
-defaultColToSave = ["FullEventId","luminosityBlock", "run","event", "sample_type", "period", "isData","PuppiMET_pt", "PuppiMET_phi", "nJet","DeepMETResolutionTune_pt", "DeepMETResolutionTune_phi","DeepMETResponseTune_pt", "DeepMETResponseTune_phi","PV_npvs"]
+JetObservablesMC = ["hadronFlavour", "partonFlavour"]
+
+FSRPhotonObservables = [
+    "FsrPhoton_dROverEt2",
+    "FsrPhoton_electronIdx",
+    "FsrPhoton_eta",
+    "FsrPhoton_muonIdx",
+    "FsrPhoton_phi",
+    "FsrPhoton_pt",
+    "FsrPhoton_relIso03",
+]
+
+SoftActivityJetObservables = [
+    "SoftActivityJet_eta",
+    "SoftActivityJet_phi",
+    "SoftActivityJet_pt",
+    "SoftActivityJetHT",
+    "SoftActivityJetHT10",
+    "SoftActivityJetHT2",
+    "SoftActivityJetHT5",
+    "SoftActivityJetNjets10",
+    "SoftActivityJetNjets2",
+    "SoftActivityJetNjets5",
+]
+
+
+defaultColToSave = [
+    "FullEventId",
+    "luminosityBlock",
+    "run",
+    "event",
+    "sample_type",
+    "period",
+    "isData",
+    "PV_npvs",
+    "BeamSpot_sigmaZ",
+    "BeamSpot_sigmaZError",
+    "BeamSpot_type",
+    "BeamSpot_z",
+    "BeamSpot_zError",
+]
+
 
 def getDefaultColumnsToSave(isData):
     colToSave = defaultColToSave.copy()
     if not isData:
-        colToSave.extend(['Pileup_nTrueInt'])
+        colToSave.extend(["Pileup_nTrueInt"])
     return colToSave
 
-def addAllVariables(dfw, syst_name, isData, trigger_class, lepton_legs, isSignal, applyTriggerFilter, global_params, channels):
-    dfw.Apply(AnaBaseline.RecoHttCandidateSelection, global_params)
+
+def addAllVariables(
+    dfw,
+    syst_name,
+    isData,
+    trigger_class,
+    lepton_legs,
+    isSignal,
+    applyTriggerFilter,
+    global_params,
+    channels,
+):
+
     dfw.Apply(AnaBaseline.LeptonVeto)
-    dfw.Apply(Corrections.getGlobal().btag.getWPid)
+
+    dfw.Apply(AnaBaseline.RecoHttCandidateSelection, global_params)
+
+    # dfw.Apply(Corrections.getGlobal().btag.getWPid)
+
     dfw.Apply(AnaBaseline.JetSelection, global_params["era"])
-    dfw.Apply(Corrections.getGlobal().jet.getEnergyResolution)
-    dfw.Apply(AnaBaseline.GetMuMuCandidate)
+
+    dfw.Apply(Corrections.getGlobal().jet.getEnergyResolution)  # nothing should change
+
+    dfw.Apply(Corrections.getGlobal().JetVetoMap.GetJetVetoMap)  # nothing should change
+
+    dfw.Apply(CommonBaseline.ApplyJetVetoMap)  # nothing should change
+
+    dfw.Apply(AnaBaseline.GetMuMuCandidate)  # nothing should change
 
     n_legs = 2
 
     for leg_idx in range(n_legs):
-        def LegVar(var_name, var_expr, var_type=None, var_cond=None, check_leg_type=True, default=0):
+
+        def LegVar(
+            var_name,
+            var_expr,
+            var_type=None,
+            var_cond=None,
+            check_leg_type=True,
+            default=0,
+        ):
             cond = var_cond
             if check_leg_type:
                 type_cond = f"HttCandidate.leg_type[{leg_idx}] != Leg::none"
                 cond = f"{type_cond} && ({cond})" if cond else type_cond
-            define_expr = f'static_cast<{var_type}>({var_expr})' if var_type else var_expr
+            define_expr = (
+                f"static_cast<{var_type}>({var_expr})" if var_type else var_expr
+            )
             if cond:
-                define_expr = f'{cond} ? ({define_expr}) : {default}'
-            dfw.DefineAndAppend( f"mu{leg_idx+1}_{var_name}", define_expr)
+                define_expr = f"{cond} ? ({define_expr}) : {default}"
+            dfw.DefineAndAppend(f"mu{leg_idx+1}_{var_name}", define_expr)
 
-        LegVar('legType', f"HttCandidate.leg_type[{leg_idx}]", check_leg_type=False)
-        for var in [ 'pt', 'eta', 'phi', 'mass' ]:
-            LegVar(var, f'HttCandidate.leg_p4[{leg_idx}].{var}()', var_type='float', default='-1.f')
-        LegVar('charge', f'HttCandidate.leg_charge[{leg_idx}]', var_type='int')
-
-        dfw.Define(f"mu{leg_idx+1}_recoJetMatchIdx", f"""HttCandidate.leg_type[{leg_idx}] != Leg::none
-                                                          ? FindMatching(HttCandidate.leg_p4[{leg_idx}], Jet_p4, 0.3)
-                                                          : -1""")
-        LegVar('iso', f"HttCandidate.leg_rawIso.at({leg_idx})")
+        LegVar("legType", f"HttCandidate.leg_type[{leg_idx}]", check_leg_type=False)
+        for var in ["pt", "eta", "phi", "mass"]:
+            LegVar(
+                var,
+                f"HttCandidate.leg_p4[{leg_idx}].{var}()",
+                var_type="float",
+                default="-1.f",
+            )
+        LegVar("charge", f"HttCandidate.leg_charge[{leg_idx}]", var_type="int")
+        LegVar(
+            f"pt_nano",
+            f"static_cast<float>(Muon_p4_nano.at(HttCandidate.leg_index[{leg_idx}]).pt())",
+        )
+        LegVar("iso", f"HttCandidate.leg_rawIso.at({leg_idx})")
 
         for muon_obs in Muon_observables:
-            if f"mu{leg_idx+1}_{muon_obs}" in dfw.df.GetColumnNames(): continue
-            if f"Muon_{muon_obs}" not in dfw.df.GetColumnNames(): continue
+            if f"mu{leg_idx+1}_{muon_obs}" in dfw.df.GetColumnNames():
+                continue
+            if f"Muon_{muon_obs}" not in dfw.df.GetColumnNames():
+                continue
 
-            LegVar(muon_obs, f"Muon_{muon_obs}.at(HttCandidate.leg_index[{leg_idx}])",
-                   var_cond=f"HttCandidate.leg_type[{leg_idx}] == Leg::mu", default='-1.f')
+            LegVar(
+                muon_obs,
+                f"Muon_{muon_obs}.at(HttCandidate.leg_index[{leg_idx}])",
+                var_cond=f"HttCandidate.leg_type[{leg_idx}] == Leg::mu",
+                default="-1.f",
+            )
         if not isData:
-            dfw.Define(f"mu{leg_idx+1}_genMatchIdx",
-                       f"HttCandidate.leg_type[{leg_idx}] != Leg::none ? HttCandidate.leg_genMatchIdx[{leg_idx}] : -1")
-            LegVar('gen_kind', f'genLeptons.at(mu{leg_idx+1}_genMatchIdx).kind()',
-                   var_type='int', var_cond=f"mu{leg_idx+1}_genMatchIdx>=0",
-                   default='static_cast<int>(GenLeptonMatch::NoMatch)')
-            LegVar('gen_charge', f'genLeptons.at(mu{leg_idx+1}_genMatchIdx).charge()',
-                   var_type='int', var_cond=f"mu{leg_idx+1}_genMatchIdx>=0", default='-10')
+            dfw.Define(
+                f"mu{leg_idx+1}_genMatchIdx",
+                f"HttCandidate.leg_type[{leg_idx}] != Leg::none ? HttCandidate.leg_genMatchIdx[{leg_idx}] : -1",
+            )
+            LegVar(
+                "gen_kind",
+                f"genLeptons.at(mu{leg_idx+1}_genMatchIdx).kind()",
+                var_type="int",
+                var_cond=f"mu{leg_idx+1}_genMatchIdx>=0",
+                default="static_cast<int>(GenLeptonMatch::NoMatch)",
+            )
+            LegVar(
+                "gen_charge",
+                f"genLeptons.at(mu{leg_idx+1}_genMatchIdx).charge()",
+                var_type="int",
+                var_cond=f"mu{leg_idx+1}_genMatchIdx>=0",
+                default="-10",
+            )
         # if not isData:
         #     for var in [ 'pt', 'eta', 'phi', 'mass' ]:
         #         LegVar(f'gen_vis_{var}', f'genLeptons.at(tau{leg_idx+1}_genMatchIdx).visibleP4().{var}()',
@@ -99,16 +397,33 @@ def addAllVariables(dfw, syst_name, isData, trigger_class, lepton_legs, isSignal
         #     LegVar(f'seedingJet_{var}', f"Jet_p4.at(tau{leg_idx+1}_recoJetMatchIdx).{var}()",
         #            var_type='float', var_cond=f"tau{leg_idx+1}_recoJetMatchIdx>=0", default='-1.f')
 
-        #Save the lep* p4 and index directly to avoid using HwwCandidate in SF LUTs
-        dfw.Define( f"mu{leg_idx+1}_p4", f"HttCandidate.leg_type.size() > {leg_idx} ? HttCandidate.leg_p4.at({leg_idx}) : LorentzVectorM()")
-        dfw.Define( f"mu{leg_idx+1}_index", f"HttCandidate.leg_type.size() > {leg_idx} ? HttCandidate.leg_index.at({leg_idx}) : -1")
-        dfw.Define( f"mu{leg_idx+1}_type", f"HttCandidate.leg_type.size() > {leg_idx} ? static_cast<int>(HttCandidate.leg_type.at({leg_idx})) : -1")
-
+        # Save the lep* p4 and index directly to avoid using HwwCandidate in SF LUTs
+        dfw.Define(
+            f"mu{leg_idx+1}_p4",
+            f"HttCandidate.leg_type.size() > {leg_idx} ? HttCandidate.leg_p4.at({leg_idx}) : LorentzVectorM()",
+        )
+        dfw.Define(
+            f"mu{leg_idx+1}_p4_nano",
+            f"Muon_p4_nano.at(HttCandidate.leg_index[{leg_idx}])",
+        )
+        dfw.Define(
+            f"mu{leg_idx+1}_index",
+            f"HttCandidate.leg_type.size() > {leg_idx} ? HttCandidate.leg_index.at({leg_idx}) : -1",
+        )
+        dfw.Define(
+            f"mu{leg_idx+1}_type",
+            f"HttCandidate.leg_type.size() > {leg_idx} ? static_cast<int>(HttCandidate.leg_type.at({leg_idx})) : -1",
+        )
 
     for jetobs in JetObservables + ["idx"]:
         jet_obs_name = f"Jet_{jetobs}"
         if jet_obs_name in dfw.df.GetColumnNames():
             dfw.DefineAndAppend(f"SelectedJet_{jetobs}", f"Jet_{jetobs}[Jet_B1]")
+    if global_params["nano_version"] == "v15":
+        if not isData:
+            dfw.colToSave.extend(PUObservables)
+        dfw.colToSave.extend(FSRPhotonObservables)
+        dfw.colToSave.extend(SoftActivityJetObservables)
 
     # pf_str = global_params["met_type"]
     # dfw.DefineAndAppend(f"met_pt_nano", f"static_cast<float>({pf_str}_p4_nano.pt())")
@@ -123,5 +438,7 @@ def addAllVariables(dfw, syst_name, isData, trigger_class, lepton_legs, isSignal
     #     dfw.DefineAndAppend(f"met_{var}", f"static_cast<float>({pf_str}_{var})")
 
     if trigger_class is not None:
-        hltBranches = dfw.Apply(trigger_class.ApplyTriggers, lepton_legs,isData, applyTriggerFilter)
+        hltBranches = dfw.Apply(
+            trigger_class.ApplyTriggers, lepton_legs, isData, applyTriggerFilter
+        )
         dfw.colToSave.extend(hltBranches)
