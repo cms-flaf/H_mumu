f00de6d02a2890a5c5eb31776a111d5ee9e16c01
Valeria D'Amante <valeriadamante@gmail.com>
2026-01-27 00:56:00 +0100

    fixed files for studies

--- Studies/validation/make_mini.py
+++ Studies/validation/make_mini.py
@@ -1,24 +1,20 @@
-#!/usr/bin/env python3
-import ROOT, sys, os, glob
 
-ROOT.EnableImplicitMT()
-if "ANALYSIS_PATH" not in os.environ:
-    raise RuntimeError("Devi avere ANALYSIS_PATH settato nell'ambiente.")
+import os
+import sys
+import ROOT
+import glob
+import argparse
+
 sys.path.append(os.environ["ANALYSIS_PATH"])
-
-from FLAF.Common.HistHelper import *
-from Analysis.H_mumu import *
 
 import FLAF.Common.Utilities as Utilities
 from FLAF.Common.Setup import Setup
-import importlib
-import argparse
+# import FLAF.Common.BaselineSelection as Baseline
+from Analysis.MuonRelatedFunctions import GetMuMuP4Observables,GetAllMuMuCorrectedPtRelatedObservables
+from Analysis.JetRelatedFunctions import JetCollectionDef
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--year", required=True)
-args = parser.parse_args()
-period = f"Run3_{args.year}"
-
+# from Corrections.Corrections import Corrections
+ROOT.EnableThreadSafety()
 
 # ---------------------------------------------------------------------
 def expand_filelist(patterns):
@@ -28,162 +24,150 @@ def expand_filelist(patterns):
     files = sorted(set(files))
     return files
 
+mu_pt_for_selection = "pt_ScaRe"
+### definitions needed for DFs
+trigger_sel = f"( HLT_singleMu && ( (mu1_{mu_pt_for_selection} > 26 && mu1_HasMatching_singleMu) || (mu2_{mu_pt_for_selection} > 26 && mu2_HasMatching_singleMu) ) ) "
+trigger_sel_withDiMuon = f"{trigger_sel} || ( HLT_diMu && ( (mu1_{mu_pt_for_selection} > 19 && mu1_HasMatching_diMu) && (mu2_{mu_pt_for_selection} > 10 && mu2_HasMatching_diMu) ) )"
+SignalRegion_def = "(m_mumu < 135 && m_mumu > 115)"
+
+ID_WPs = {
+    "looseId":"mu{idx}_looseId",
+    "mediumId":"mu{idx}_mediumId",
+    "tightId":"mu{idx}_tightId",
+    "mediumMVAMuId":"mu{idx}_mvaMuID_WP == 1",
+    "tightMVAMuId":"mu{idx}_mvaMuID_WP == 2",
+    "looseLowPtMVA":"mu{idx}_mvaLowPt > -0.6",
+    "mediumLowPtMVA":"mu{idx}_mvaLowPt > -0.2",
+    "tightLowPtMVA":"mu{idx}_mvaLowPt > 0.15",
+}
+
+Iso_WPs = {
+    "pfRelIso_loose": "mu{idx}_pfRelIso04_all < 0.25",
+    "pfRelIso_medium": "mu{idx}_pfRelIso04_all < 0.2",
+    "pfRelIso_tight": "mu{idx}_pfRelIso04_all < 0.15",
+    "tkRelIso_loose":"mu{idx}_tkRelIso < 0.1",
+    "tkRelIso_tight":"mu{idx}_tkRelIso < 0.05",
+    "miniPFRelIso_loose":"mu{idx}_miniPFRelIso_all < 0.4",
+    "miniPFRelIso_medium":"mu{idx}_miniPFRelIso_all < 0.2",
+    "miniPFRelIso_tight":"mu{idx}_miniPFRelIso_all < 0.1",
+    "multiIso_loose":"mu{idx}_multiIsoId == 1",
+    "multiIso_medium":"mu{idx}_multiIsoId == 2",
+}
+
+all_cuts_mu1 = {}
+all_cuts_mu2 = {}
+for idCut_name,idCut_def in ID_WPs.items():
+    for isoCut_name,isoCut_def in Iso_WPs.items():
+        all_cuts_mu1[f"mu1_{idCut_name}_{isoCut_name}"]= idCut_def.format(idx=1) + " && " + isoCut_def.format(idx=1)
+        all_cuts_mu2[f"mu2_{idCut_name}_{isoCut_name}"]= idCut_def.format(idx=2) + " && " + isoCut_def.format(idx=2)
+
+def GetAllDfStuff(input_df, outFileName):
+    input_df = input_df.Define(
+        "isVBF",
+        'std::string(ROOT::RDF::RSampleInfo().AsString()).find("VBFHto2Mu") != std::string::npos',
+    )
+    input_df = input_df.Define(
+        "isggH",
+        'std::string(ROOT::RDF::RSampleInfo().AsString()).find("ggHto2Mu") != std::string::npos',
+    )
+    input_df = GetMuMuP4Observables(input_df)
+    input_df = GetAllMuMuCorrectedPtRelatedObservables(input_df)
+    input_df = JetCollectionDef(input_df)
+    # -------------------------
+    # Define all WP combinations
+    # -------------------------
+    wp_columns = []
+    columns = [
+        "weight_MC_Lumi_pu",
+        "isVBF",
+        "isggH",
+        # "Signal_Fit",
+        # "JetTagSel",
+        # "m_mumu",
+    ]
+
+    for mu1_colName,mu1_colExpr in all_cuts_mu1.items():
+        if mu1_colName not in input_df.GetColumnNames():
+            input_df = input_df.Define(mu1_colName, mu1_colExpr)
+        for mu2_colName,mu2_colExpr in all_cuts_mu2.items():
+            if mu2_colName not in input_df.GetColumnNames():
+                input_df = input_df.Define(mu2_colName, mu2_colExpr)
+            colname = f"{mu1_colName}_{mu2_colName}"
+            # print(f"defining {colname}")
+            input_df = input_df.Define(
+                f"{colname}",
+                f"{mu1_colName} && {mu2_colName}",
+            )
+            columns.append(colname)
+    # -------------------------
+    # Columns to save
+    # -------------------------
+    df_singleMuonOnly = input_df.Filter(f"({trigger_sel} && JetTagSel && {SignalRegion_def} )")
+    df_singleAndDiMuonTrg = input_df.Filter(f"({trigger_sel_withDiMuon}) && JetTagSel && {SignalRegion_def}")
+    return df_singleMuonOnly,df_singleAndDiMuonTrg,columns
+
+### setup everything
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--year", required=False, default="2022")
+args = parser.parse_args()
+period = f"Run3_{args.year}"
+
+setup = Setup.getGlobal(os.environ["ANALYSIS_PATH"], period)
+histTupleDef = Utilities.load_module(os.path.join(os.environ["ANALYSIS_PATH"],"Analysis/histTupleDef.py"))
+histTupleDef.Initialize()
+histTupleDef.analysis_setup(setup)
+
+### input files
+input_path = f"/eos/user/e/eusebi/Hmumu/anaTuples/MuID_Iso_Studies_diMuonTrg/{period}"
+output_path = f"/afs/cern.ch/work/v/vdamante/H_mumu/Studies/validation/new_miniTuples/{period}"
+input_df = ROOT.RDataFrame("Events",f"/eos/user/v/vdamante/H_mumu/anaTuples/test_PR/Run3_2022/data/anaTuple_0.root")
 
 sig_patterns = [
-    f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/GluGluHto2Mu/anaTuple*.root",
-    f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/VBFHto2Mu/anaTuple*.root",
+    f"{input_path}/GluGluHto2Mu/anaTuple*.root",
+    f"{input_path}/VBFHto2Mu/anaTuple*.root",
 ]
 
-ggh_files = expand_filelist(
-    [
-        f"/eos/user/e/eusebi/Hmumu/anaTuples/v5_diMuonTriggerIncluded/{period}/GluGluHto2Mu/split/nano*.root"
-    ]
-)
-
-vbf_files = expand_filelist(
-    [
-        f"/eos/user/e/eusebi/Hmumu/anaTuples/v5_diMuonTriggerIncluded/{period}/VBFHto2Mu/split/nano*.root"
-    ]
-)
-
-
-# bkg_patterns = [
-#     f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/DYto2E_M_50_0J_amcatnloFXFX/anaTuple*.root",
-#     f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/DYto2E_M_50_1J_amcatnloFXFX/anaTuple*.root",
-#     f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/DYto2E_M_50_2J_amcatnloFXFX/anaTuple*.root",
-#     f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/DYto2Mu_M_50_0J_amcatnloFXFX/anaTuple*.root",
-#     f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/DYto2Mu_M_50_1J_amcatnloFXFX/anaTuple*.root",
-#     f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/DYto2Mu_M_50_2J_amcatnloFXFX/anaTuple*.root",
-#     f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/DYto2Tau_M_50_0J_amcatnloFXFX/anaTuple*.root",
-#     f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/DYto2Tau_M_50_1J_amcatnloFXFX/anaTuple*.root",
-#     f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/DYto2Tau_M_50_2J_amcatnloFXFX/anaTuple*.root"
-# ]
-
-bkg_patterns = [
-    f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/TTto2L2Nu/anaTuple*.root",
-    f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/TTto4Q/anaTuple*.root",
-    f"/eos/user/e/eusebi/Hmumu/anaTuples/v4_20Nov_NewJetIDs_LooseMuons/{period}/TTtoLNu2Q/anaTuple*.root",
+DY_patterns = [
+    f"{input_path}/DYto2E_M_50_0J_amcatnloFXFX/anaTuple*.root",
+    f"{input_path}/DYto2E_M_50_1J_amcatnloFXFX/anaTuple*.root",
+    f"{input_path}/DYto2E_M_50_2J_amcatnloFXFX/anaTuple*.root",
+    f"{input_path}/DYto2Mu_M_50_0J_amcatnloFXFX/anaTuple*.root",
+    f"{input_path}/DYto2Mu_M_50_1J_amcatnloFXFX/anaTuple*.root",
+    f"{input_path}/DYto2Mu_M_50_2J_amcatnloFXFX/anaTuple*.root",
+    f"{input_path}/DYto2Tau_M_50_0J_amcatnloFXFX/anaTuple*.root",
+    f"{input_path}/DYto2Tau_M_50_1J_amcatnloFXFX/anaTuple*.root",
+    f"{input_path}/DYto2Tau_M_50_2J_amcatnloFXFX/anaTuple*.root"
 ]
 
-sig_files = [f for p in sig_patterns for f in glob.glob(p)]
-bkg_files = [f for p in bkg_patterns for f in glob.glob(p)]
-all_sig_files = vbf_files + ggh_files
-setup = Setup.getGlobal(os.environ["ANALYSIS_PATH"], period, "")
-analysis_import = setup.global_params["analysis_import"]
-analysis = importlib.import_module(analysis_import)
+TT_patterns = [
+    f"{input_path}/TTto2L2Nu/anaTuple*.root",
+    f"{input_path}/TTto4Q/anaTuple*.root",
+    f"{input_path}/TTtoLNu2Q/anaTuple*.root",
+]
 
-gp_sig = dict(setup.global_params)
-gp_sig["process_name"] = setup.samples["VBFHto2Mu"]["process_name"]
-gp_sig["process_group"] = setup.samples["VBFHto2Mu"]["process_group"]
-
-gp_bkg = dict(setup.global_params)
-gp_bkg["process_name"] = "DY"
-gp_bkg["process_group"] = "DY"
-
-kwargset = {}
-kwargset["isData"] = False
-kwargset["wantTriggerSFErrors"] = False
-kwargset["colToSave"] = []
+W_patterns = [
+    f"{input_path}/WtoLNu_0J_amcatnloFXFX/anaTuple*.root",
+    f"{input_path}/WtoLNu_1J_amcatnloFXFX/anaTuple*.root",
+    f"{input_path}/WtoLNu_2J_amcatnloFXFX/anaTuple*.root",
+]
+# 1. select only single muon trigger, look at other ID/Iso vars:
 
 full_sig_list = expand_filelist(sig_patterns)
-full_bkg_list = expand_filelist(bkg_patterns)
+full_DY_list = expand_filelist(DY_patterns)
+full_TT_list = expand_filelist(TT_patterns)
+full_W_list = expand_filelist(W_patterns)
 
-print(
-    f"Found {len(full_sig_list)} signal files, {len(full_bkg_list)} background files."
-)
+samples_names_list = {"sig":full_sig_list, "DY": full_DY_list, "TT":full_TT_list, "W":full_W_list}
 
-if len(full_sig_list) == 0 or len(full_bkg_list) == 0:
-    raise RuntimeError("File lists vuote: controlla i pattern passati.")
+for sample_name,samples_list in samples_names_list.items():
+    print(f"processing {sample_name}")
+    inputDataFrame = ROOT.RDataFrame("Events", Utilities.ListToVector(samples_list))
+    output_file = f"{output_path}/mini_{sample_name}.root"
+    df_singleMuonOnly,df_singleAndDiMuonTrg,columns = GetAllDfStuff(inputDataFrame, output_file)
+    df_singleMuonOnly.Snapshot("singleMu",output_file,columns)
+    df_singleAndDiMuonTrg.Snapshot("singleMuOrDiMuon",output_file,columns)
+    print(f"Mini-ntupla salvata in {output_file}")
 
 
-rdf_sig = ROOT.RDataFrame("Events", Utilities.ListToVector(all_sig_files))
 
-rdf_sig = rdf_sig.Define(
-    "isVBF",
-    'std::string(ROOT::RDF::RSampleInfo().AsString()).find("VBFHto2Mu") != std::string::npos',
-)
-rdf_sig = rdf_sig.Define(
-    "isggH",
-    'std::string(ROOT::RDF::RSampleInfo().AsString()).find("ggHto2Mu") != std::string::npos',
-)
-# rdf_bkg = ROOT.RDataFrame("Events", Utilities.ListToVector(full_bkg_list))
-# rdf_bkg = rdf_bkg.Define("isVBF","0")
-# rdf_bkg = rdf_bkg.Define("isggH","0")
-
-dfw_sig = analysis.DataFrameBuilderForHistograms(rdf_sig, gp_sig, period, **kwargset)
-# dfw_bkg = analysis.DataFrameBuilderForHistograms(rdf_bkg, gp_bkg, period, **kwargset)
-
-for dfw in [dfw_sig]:  # , dfw_bkg):
-    dfw.df = Utilities.defineP4(dfw.df, f"mu1")
-    dfw.df = Utilities.defineP4(dfw.df, f"mu2")
-    dfw.df = dfw.df.Define("m_mumu", f"(mu1_p4 + mu2_p4).M()")
-    dfw.defineChannels()
-    dfw.defineTriggers()
-
-    # dfw.df = AddNewDYWeights(dfw.df, dfw.period, False)
-    # dfw.df = AddMuTightIDWeights(dfw.df, dfw.period)
-    # dfw.df = dfw.df.Define("Jet_vetoMap","Jet_pt > 15 && ( Jet_passJetIdTightLepVeto ) && (Jet_chEmEF + Jet_neEmEF < 0.9) && Jet_isInsideVetoRegion")
-    dfw.df = JetCollectionDef(dfw.df)
-    dfw.df = VBFJetSelection(dfw.df)
-    dfw.SignRegionDef()
-    dfw.defineRegions()
-    dfw.defineCategories()
-    dfw.df = dfw.df.Define(
-        f"baseline_noID_Iso_withDiMuonTrg",
-        "OS && (trigger_sel || ( (mu1_pt > 19 && mu1_HasMatching_diMu) && (mu2_pt > 10 && mu2_HasMatching_diMu) ) )",
-    )
-    dfw.df = dfw.df.Define(f"baseline_noID_Iso", "OS && trigger_sel")
-    dfw.df = dfw.df.Define(
-        f"VBF_JetVeto_noID_Iso",
-        "baseline_noID_Iso && HasVBF && j1_pt >= 35 && j2_pt >= 25",
-    )
-    dfw.df = dfw.df.Define(
-        f"ggH_noID_Iso", "baseline_noID_Iso && !(VBF_JetVeto_noID_Iso)"
-    )
-    dfw.df = dfw.df.Define(
-        f"mu1_isSingleMu", "(mu1_pt > 26 && mu1_HasMatching_singleMu)"
-    )
-    dfw.df = dfw.df.Define(
-        f"mu2_isSingleMu", "(mu2_pt > 26 && mu2_HasMatching_singleMu)"
-    )
-    ## assuming muons ordered by pT ##
-    dfw.df = dfw.df.Define(f"mu1_isdiMu", "(mu1_pt > 19 && mu1_HasMatching_diMu)")
-    dfw.df = dfw.df.Define(f"mu2_isdiMu", "(mu2_pt > 10 && mu1_HasMatching_diMu)")
-
-    # dfw.df = dfw.df.Define("Signal_Fit", "m_mumu > 115 && m_mumu < 135")
-    # dfw.df = dfw.df.Define("looseID_mu1_looseID_mu2",  "mu1_looseId  && mu2_looseId")
-    # dfw.df = dfw.df.Define("looseID_mu1_mediumID_mu2",  "mu1_looseId  && mu2_mediumId")
-    # dfw.df = dfw.df.Define("mediumID_mu1_looseID_mu2",  "mu1_mediumId  && mu2_looseId")
-    # dfw.df = dfw.df.Define("mediumID_mu1_mediumID_mu2",  "mu1_mediumId  && mu2_mediumId")
-    # dfw.df = dfw.df.Define("tightID_mu1_tightID_mu2",  "mu1_tightId  && mu2_tightId")
-
-cols_to_save = [
-    "Signal_Fit",
-    "baseline_noID_Iso",
-    "baseline_noID_Iso_withDiMuonTrg",
-    "VBF_JetVeto_noID_Iso",
-    "ggH_noID_Iso",
-    "weight_MC_Lumi_pu",
-    "isggH",
-    "isVBF",
-]
-
-for muCol in [
-    "looseId",
-    "mediumId",
-    "tightId",
-    "pfRelIso04_all",
-    "isSingleMu",
-    "isdiMu",
-]:
-    for muIdx in [1, 2]:
-        cols_to_save.append(f"mu{muIdx}_{muCol}")
-
-dfw_sig.df.Snapshot(
-    "Mini",
-    f"/afs/cern.ch/work/v/vdamante/H_mumu/Studies/validation/stuff/mini_signal_{args.year}_diMuonTrg.root",
-    cols_to_save,
-)
-# dfw_bkg.df.Snapshot("Mini", f"/afs/cern.ch/work/v/vdamante/H_mumu/Studies/validation/stuff/mini_bkgTTbar_{args.year}.root", cols_to_save)
-
-print("Mini-ntupla salvata. Pronta per calcolo efficienze.")
